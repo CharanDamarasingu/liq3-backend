2a7d06fd5a93aad2a3c4e050cafbc5ff8677a210
CharanDamarasingu <cherryrr79@gmail.com>
2026-04-26 11:38:55 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 // 2. Connect to your local MongoDB
-mongoose.connect('mongodb+srv://cherryrr79:Qwerty@2679@cluster0.hpaiftt.mongodb.net/')
+mongoose.connect('mongodb+srv://cherryrr79:Qwerty@2679@cluster0.hpaiftt.mongodb.net/liquid3_db/')
   .then(() => console.log('Connected to MongoDB!'))
   .catch(err => console.error('Failed to connect to MongoDB:', err));
 
